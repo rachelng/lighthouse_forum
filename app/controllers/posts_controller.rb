30719cb09d9ddd3f5cd63d813c4ba4d5071af4b2
Rachel Ng <racehl@rachel-ng.com>
2014-01-26 23:17:57 +0000

    Keeping files DRY

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,44 +35,38 @@ class PostsController < ApplicationController
 	end
 
 
-	def post0
-		@post = {
-			title: "title",
-			author: "author",
-			text: <<-eos.gsub(/\s+/, " ").strip
-				Blah blah blah blah
-			eos
-		}
+	def show
+		posts = [
+			{
+				title: "title",
+				author: "author",
+				text: <<-eos.gsub(/\s+/, " ").strip
+					Blah blah blah blah
+				eos
+			},
+			{
+				title: "title",
+				author: "author",
+				text: <<-eos.gsub(/\s+/, " ").strip
+					Blah blah blah blah
+				eos
+			},
+			{
+				title: "title",
+				author: "author",
+				text: <<-eos.gsub(/\s+/, " ").strip
+					Blah blah blah blah
+				eos
+			},
+			{
+				title: "title",
+				author: "author",
+				text: <<-eos.gsub(/\s+/, " ").strip
+					Blah blah blah blah
+				eos
+			}
+		]	
+		@post = posts[params[:id].to_i]
 	end
-
-	def post1
-		@post = {
-			title: "title",
-			author: "author",
-			text: <<-eos.gsub(/\s+/, " ").strip
-				Blah blah blah blah
-			eos
-		}
-	end
-
-	def post2
-		@post = {
-			title: "title",
-			author: "author",
-			text: <<-eos.gsub(/\s+/, " ").strip
-				Blah blah blah blah
-			eos
-		}
-	end
-
-	def post3
-		@post = {
-			title: "title",
-			author: "author",
-			text: <<-eos.gsub(/\s+/, " ").strip
-				Blah blah blah blah
-			eos
-		}
-	end	
 
 end
