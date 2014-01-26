f3021088f5bb569288d531273e62dc15e89c7f2d
Rachel Ng <racehl@rachel-ng.com>
2014-01-26 23:00:30 +0000

    Created post template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,37 @@
 class PostsController < ApplicationController
 
 	def index
+		@posts = [
+			{
+				title: "title",
+				author: "author",
+				text: <<-eos.gsub(/\s+/, " ").strip
+					Blah blah blah blah
+				eos
+			},
+			{
+				title: "title",
+				author: "author",
+				text: <<-eos.gsub(/\s+/, " ").strip
+					Blah blah blah blah
+				eos
+			},
+			{
+				title: "title",
+				author: "author",
+				text: <<-eos.gsub(/\s+/, " ").strip
+					Blah blah blah blah
+				eos
+			},
+			{
+				title: "title",
+				author: "author",
+				text: <<-eos.gsub(/\s+/, " ").strip
+					Blah blah blah blah
+				eos
+			}
+		]						
+
 	end
 
 end
